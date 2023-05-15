e002eee0c615a2cf8a5962a76e5961672b3f79d3
Faisal <md.faisal1899@yahoo.com>
2023-05-15 20:33:11 +0600

    refactor code

--- app.js
+++ app.js
@@ -3,34 +3,9 @@ const express = require('express');
 
 const app = express();
 
-const contents = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/contents.json`)
-);
+const contentController = require('./controllers/contentControllers');
 
-app.get('/api/v1/contents', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        result: contents.length,
-        data: {
-            contents,
-        },
-    });
-});
+app.route('/api/v1/contents').get(contentController.getAllContents);
+app.route('/api/v1/contents/:id').get(contentController.getContent);
 
-app.get('/api/v1/contents/:id', (req, res) => {
-    const id = req.params.id * 1;
-
-    const content = contents.find((c) => c.id === id);
-    console.log(content);
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            content,
-        },
-    });
-});
-
-app.listen(3000, () => {
-    console.log('server is running...');
-});
+module.exports = app;
---
+++ controllers/contentControllers.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const contents = JSON.parse(
+    fs.readFileSync(`${__dirname}/../dev-data/data/contents.json`)
+);
+
+exports.getAllContents = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: contents.length,
+        data: {
+            contents,
+        },
+    });
+};
+
+exports.getContent = (req, res) => {
+    const id = req.params.id * 1;
+
+    const content = contents.find((c) => c.id === id);
+    console.log(content);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            content,
+        },
+    });
+};
