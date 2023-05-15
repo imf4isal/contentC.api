917b0ce244a7ff8f6583f228bb1cafd81e2e9d2d
Faisal <md.faisal1899@yahoo.com>
2023-05-15 20:03:10 +0600

    test. implement functionality for getting all data

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const contents = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/contents.json`)
+);
+
+app.get('/api/v1/contents', (req, res) => {
     res.status(200).json({
         status: 'success',
+        result: contents.length,
+        data: {
+            contents,
+        },
     });
 });
 
